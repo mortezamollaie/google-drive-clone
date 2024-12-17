a2d51c68ebee94618892a2cd5ba4ec9a45490da3
mortezamollaie <mm.gov.1381@gmail.com>
2024-12-17 10:00:31 +0330

    send email for shared files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -416,7 +416,7 @@ class FileController extends Controller
         }
         FileShare::insert($data);
 
-        // Mail::to($user)->send(new ShareFilesMail($user, Auth::user(), $files));
+        Mail::to($user)->send(new ShareFilesMail($user, Auth::user(), $files));
 
         return redirect()->back();
     }
